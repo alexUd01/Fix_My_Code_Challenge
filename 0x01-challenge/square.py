81ce715e6403abbc4227fabfbb95e9e208404b7b
alexUd01 <alexanderikpeama@gmail.com>
2023-04-12 13:45:26 +0100

    Square: correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,85 +1,33 @@
 #!/usr/bin/python3
-""" Square module: challenge """
+""" Module for square class"""
 
 
 class Square():
-    """ Square Class """
-
-    __width = 0
-    __height = 0
+    """ Square class """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initializations """
-        if args:
-            try:
-                self.width = args[0]
-                self.height = args[1]
-            except IndexError:
-                raise  # pass
-        elif kwargs:
-            self.width = kwargs.get('width', 0)
-            self.height = kwargs.get('height', 0)
-
-    @property
-    def width(self):
-        """ Getter """
-        return self.__width
-
-    @width.setter
-    def width(self, val):
-        """ Setter """
-        if type(val) is int:
-            if val < 0:
-                raise ValueError('width must be >= 0')
-            self.__width = val
-        else:
-            raise TypeError('width must be an integer')
-
-    @property
-    def height(self):
-        """ Getter """
-        return self.__height
-
-    @height.setter
-    def height(self, val):
-        """ Setter """
-        if type(val) is int:
-            if val < 0:
-                raise ValueError('width must be >= 0')
-            self.__height = val
-        else:
-            raise TypeError('height must be an integer')
+        """ Instantiation of class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.__width * self.__height
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Compute Perimeter """
-        return 2 * (self.__width  + self.__height)
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Pretty print """
-        return "{}/{}".format(self.__width, self.__height)
+        """ Printable representation """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
-
-    s2 = Square(1, -1)
-    s2.width = 500
-    print(s2)
-    print(s2.area_of_my_square())
-
-    s3 = Square()
-    print(s3)
-
-    print()
-
-    print(Square(4, 3))
-    print(Square(4, 3).area_of_my_square())
-    print(Square(4, 3).PermiterOfMySquare())
+    print(s.permiter_of_my_square())
